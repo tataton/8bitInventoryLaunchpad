f22f52af2ad597eb6d2db19c25b7f21ce93d8d69
devjanaprime <dev@primeacademy.io>
2016-11-17 11:22:47 -0600

    added findObject function and preliminary functionality

--- public/scripts/8bitInventory.js
+++ public/scripts/8bitInventory.js
@@ -1,19 +1,42 @@
-console.log( 'js' );
 // properties by which searches can be done
 var sizes = [ 'small', 'medium', 'large' ];
 var colors = [ 'red', 'orange', 'yellow', 'green', 'mermaid treasure', 'blue', 'purple' ];
 
-////// example object //////
-var testObject = {
-  color: 'blue',
-  name: 'blueberry',
-  size: 'small'
-}; // end testObject
-
 ////// global array of items in inventory //////
 var items = [];
-items.push( testObject);
 
 $( document ).ready( function(){
-  console.log( 'JQ' );
+  var findObject = function( colorCheck, sizeCheck ){
+    console.log( 'in findObject. Looking for:', colorCheck, sizeCheck );
+    // array of matches
+    var matches = [];
+    for ( var i = 0; i < items.length; i++ ) {
+      if( items[i].color == colorCheck && items[i].size == sizeCheck ){
+        // match, add to array
+        matches.push( items[i] );
+      } // end if
+    } // end for
+    // return array of matches
+    return matches;
+  }; // end findObject
+
+  var getObjects = function(){
+    console.log( 'in getObjects');
+    // populate the items array
+    ////// replace the stuff in this function with getting items from the database ////////
+    ////// example object //////
+    var testObject = {
+      color: 'blue',
+      name: 'blueberry',
+      size: 'small'
+    }; // end testObject
+    items.push( testObject );
+  }; // end getObjects
+
+  // get objects when doc is ready
+  getObjects();
+  // the below are tests to show what is returned when running findObject
+  console.log( findObject( 'blue', 'small' ) );
+  console.log( findObject( 'blue', 'large' ) );
+
 }); // end doc ready
