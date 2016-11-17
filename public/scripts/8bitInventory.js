8b2284ee473a0cb1a3518a3bc2bb61190b880b00
devjanaprime <dev@primeacademy.io>
2016-11-17 11:08:00 -0600

    added test obejct and global items array

--- public/scripts/8bitInventory.js
+++ public/scripts/8bitInventory.js
@@ -1,4 +1,18 @@
 console.log( 'js' );
+// properties by which searches can be done
+var sizes = [ 'small', 'medium', 'large' ];
+var colors = [ 'red', 'orange', 'yellow', 'green', 'mermaid treasure', 'blue', 'purple' ];
+
+////// example object //////
+var testObject = {
+  color: 'blue',
+  name: 'blueberry',
+  size: 'small'
+}; // end testObject
+
+////// global array of items in inventory //////
+var items = [];
+items.push( testObject);
 
 $( document ).ready( function(){
   console.log( 'JQ' );
