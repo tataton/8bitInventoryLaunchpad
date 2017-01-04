40cbd68925a03d5680a77bf0a69e3f3ba698aedc
T. Andrew Taton <thomas.andrew.taton@gmail.com>
2017-01-04 08:09:45 -0600

    Some of this done.

---
+++ database_instruction.sql
@@ -0,0 +1,6 @@
+CREATE TABLE scarves (
+    id SERIAL PRIMARY KEY,
+    name varchar(30),
+    size varchar(20),
+    color varchar(20)
+);
--- server/app.js
+++ server/app.js
@@ -4,29 +4,50 @@ var path = require( 'path' );
 var bodyParser = require( 'body-parser' );
 var urlEncodedParser = bodyParser.urlencoded( { extended: false } );
 var port = process.env.PORT || 3003;
+var pg = require('pg');
+app.use(express.static('public'));
 
-// spin up server
-app.listen( port, function(){
-  console.log( 'server up on:', port );
+var connectionString = 'postgres://localhost:5432/dev_scarves';
+
+app.listen(port, function(){
+  console.log('Server up on port:', port);
 }); // end spin up server
 
 // base url
-app.get( '/', function( req, res ){
-  console.log( 'base url hit' );
-  res.sendFile( path.resolve( 'views/index.html' ) );
+app.get('/', function(req, res){
+  console.log('base url hit');
+  res.sendFile(path.resolve('views/index.html'));
 }); // end home base
 
-// add new objects to the inventory
-app.post( '/addItem', urlEncodedParser, function( req, res ){
-  console.log( 'addItem route hit:', req.body );
-  // add the item from req.body to the table
+app.post('/addItem', urlEncodedParser, function(req, res){
+  console.log('addItem route hit:', req.body);
+  pg.connect(connectionString, function(err, client, done) {
+    if (err) {
+      console.log(err);
+    } else {
+      client.query('INSERT INTO scarves (first_name, last_name) VALUES ($1, $2);', [req.body.first_name, req.body.last_name]);
+      done();
+      res.sendStatus(201);
+    }
+  });
 }); // end addItem route
 
-// get all objects in the inventory
-app.get( '/getInventory', function( req, res ){
-  console.log( 'getInventory route hit' );
+app.get('/getInventory', function(req, res){
+  pg.connect(connectionString, function(err, client, done) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Connected to database from getInventory.');
+      var allScarves = [];
+      var query = client.query('SELECT * FROM scarves');
+      query.on('row', function(row){
+        allScarves.push(row);
+      }); // end query
+      query.on('end', function(){
+        done();
+        res.send(allScarves);
+      });
+    }
+  });
   // get all items in the table and return them to client
 }); // end addItem route
-
-// static folder
-app.use( express.static( 'public' ) );
